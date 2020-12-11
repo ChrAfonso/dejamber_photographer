094b9c93085e85b0d8ba1db4b84859cb53ba427b
Rotter <71013884+NordiAut@users.noreply.github.com>
2020-12-11 22:38:50 +0100

    Cooldown/Mood/RefillCooldown/RefillMood

---
+++ Assets/Scripts/Mood.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mood : MonoBehaviour
+{
+
+    public float MoodValue = 10;
+    private float leftMood = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftMood = MoodValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (leftMood > 0)
+        {
+            leftMood -= Time.deltaTime;
+        }
+    }
+
+    public float getMood()
+    {
+        return leftMood / MoodValue;
+    }
+}
