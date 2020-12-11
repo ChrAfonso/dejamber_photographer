6fe93e81603f8fc1fff04857d0ef5d2c91dbe155
Rotter <71013884+NordiAut@users.noreply.github.com>
2020-12-11 19:46:18 +0100

    Cooldown added

---
+++ Assets/Scripts/Cooldown.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cooldown : MonoBehaviour
+{
+    public string ActionAtEnd = "";
+    public float DurationCD = 5;
+
+    private float leftTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftTime = DurationCD;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftTime>0)
+        {
+            leftTime -= Time.deltaTime;
+        }
+        else
+        {
+            leftTime = 0;
+            Debug.Log("Ist angekommen. - Cooldown finished");
+            MonoBehaviour action = gameObject.GetComponent<MoveToBuffet>();
+            action.enabled = true;
+            this.enabled = false;
+        }
+
+        
+    }
+}
