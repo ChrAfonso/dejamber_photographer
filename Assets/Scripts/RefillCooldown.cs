3cfe73f3c233f71ba30e0d60b1288423bcc9bae2
Rotter <71013884+NordiAut@users.noreply.github.com>
2020-12-11 22:03:39 +0100

    Cooldown & Progress Bar Update

--- Assets/Scripts/RefillCooldown.cs
+++ Assets/Scripts/RefillCooldown.cs
@@ -1,39 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
 
-public class Cooldown : MonoBehaviour
+public class RefillCooldown : MonoBehaviour
 {
     public string ActionAtEnd = "";
     public float DurationCD = 5;
-    
 
     private float leftTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        leftTime = DurationCD;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (leftTime>0)
+        if (leftTime < DurationCD)
         {
-            leftTime -= Time.deltaTime;
+            leftTime += Time.deltaTime;
         }
         else
         {
-            leftTime = 0;
-            Debug.Log("Ist angekommen. - Cooldown finished");
+            leftTime = DurationCD;
+            Debug.Log("Ist wieder aufgefüllt. - Cooldown finished");
             MonoBehaviour action = gameObject.GetComponent<MoveToBuffet>();
             action.enabled = true;
             this.enabled = false;
         }
 
-        
+
     }
     public float getValue()
     {
