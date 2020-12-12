6feed1e3105861f0746b025c15c2910172554c11
Christian Afonso <christian.afonso@wohlhart.at>
2020-12-12 14:07:25 +0100

    Added DEBUG_StateLabel to girl,mum prefabs

---
+++ Assets/Scripts/DEBUG_StateLabel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DEBUG_StateLabel : MonoBehaviour
+{
+    private TextMeshPro textMesh;
+    private PersonController personController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = gameObject.GetComponent<TextMeshPro>();
+        personController = gameObject.GetComponentInParent<PersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetText(personController.playerState.ToString());
+    }
+
+    public void SetText(string text)
+    {
+        textMesh.text = text;
+    }
+
+    public void SetColor(Color color)
+    {
+        textMesh.color = color;
+    }
+}
